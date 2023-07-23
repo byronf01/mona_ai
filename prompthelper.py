8e25ccd88d4c6ffac2bcf69c07a0ee6d0453ca13
Byron Fong <byron.fong@gmail.com>
2023-07-22 23:55:26 -0700

    gn

--- prompthelper.py
+++ prompthelper.py
@@ -2,6 +2,7 @@
 # Pre-processes and labels input/output data for the SFT dataset
 """
 import re
+import sys
 
 # Constants mostly for reference
 START_OF_SENTENCE = '|<sos>|' # start of sentence, starts reading for answer
@@ -103,7 +104,7 @@ def v1():
         if not f.closed:
             f.close()
 
-def v2():
+def v2(debug: bool):
     """
     Second version of data pre-processing from a text file. Still will require manual input to mark start and stop between conversation.
     Requires text from character ai to be put into something like Google Docs and all the italicized text removed before copying into the 
@@ -112,7 +113,7 @@ def v2():
     with open("c_ai.txt", 'rb') as f:
         text = f.read()
     text = text.decode(encoding='utf-8')
-    m1 = r'(\r\nhiyahhhhh\r\n|\r\n\r\nMona\r\nc.ai\r\n)'
+    m1 = r'([\r\n]*hiyahhhhh\r\n|[\r\n]*Mona\r\nc.ai\r\n)'
     data = [resp for resp in re.split(m1, text) if re.match(m1, resp) == None and resp != '']
 
     prompts_start = 0
@@ -123,41 +124,61 @@ def v2():
         contents = contents.decode(encoding='utf-8')
     prompts_start = len(contents.split(END_OF_CONVERSATION)) - 1
 
-    print('INSTRUCTIONS: ')
-    print(f'This program will prompt you for two parts of a conversation: a PROMPT and ANSWER. Finish entering prompt and then ' + 
-          f'enter {END_OF_CONVERSATION} to finish entering the answer and stop the conversation. {END_OF_SENTENCE} ' + 
-          f'can be entered in place of {END_OF_CONVERSATION} to continue the conversation with more prompts and answers as necessary. ')
-    print()
-
     # Check if data is valid
     if len(data) % 2: 
         raise AssertionError('Unequal amount of prompts and responses')
     
     current_conversation = START_OF_CONVERSATION
     i = 0 # Iterator for all tokens
-    while i < len(data):
-        print(data[i])
-        current_conversation += (PROMPT_START + data[i] + PROMPT_END)
-        i += 1
-        print(data[i])
-        current_conversation += (START_OF_SENTENCE + data[i] + END_OF_SENTENCE)
 
-        if i+1 < len(data):
-            print(f'Next prompt preview: {data[i+1][0:15] if len(data[i+1]) < 15 else data[i+1]}')
-        end = input('End of Conversation? (Y )')
-
-        if end == '':
-            # Conversation continues
-            pass
-        else:
-            current_conversation += END_OF_CONVERSATION
-            current_conversation = '\n' + current_conversation
-            conversation = conversation.encode(encoding='utf-8')
-            with open('prompts.txt', 'ab') as f:
+    try:
+        while i < len(data):
+            print()
+            print(f'YOU: \t' + data[i])
+            current_conversation += (PROMPT_START + data[i] + PROMPT_END)
+            i += 1
+            print(f'MONA: \t' + data[i])
+            current_conversation += (START_OF_SENTENCE + data[i] + END_OF_SENTENCE)
+            i += 1
+            
+            end = 'foo'
+            if i < len(data):
+                end = input(f'\nEnd of Conversation? Preview: {data[i][:50] if len(data[i]) > 50 else data[i]} (Y/Empty) ')
                 
-                f.write(conversation)
-                new_prompts += 1
+            
+            if end == '':
+                # Conversation continues
+                pass
+            else:
+                current_conversation += END_OF_CONVERSATION
+                current_conversation = '\n' + current_conversation
+                print('FULL CONVERSATION: MAKE SURE TO SANITY CHECK !!!' + current_conversation)
+                current_conversation = current_conversation.encode(encoding='utf-8')
+                with open('prompts.txt', 'ab') as f:
+                    
+                    if not debug:
+                        f.write(current_conversation)
+                    new_prompts += 1
+
+                current_conversation = START_OF_CONVERSATION
+
+    except KeyboardInterrupt:
+        
+        if debug:
+            new_prompts = 0
+
+    finally:
+        if not f.closed:
+            f.close()
+        print()
+        print('----- SESSION RESULTS ----- ')
+        print(f'OLD PROMPTS: \t{str(prompts_start)}')
+        print(f'NEW PROMPTS: \t{str(new_prompts)}')
+        print(f'TOTAL PROMPTS: \t{str(prompts_start + new_prompts)}')
 
 if __name__ == '__main__':
-
-    v2()
+    
+    if 'debug' in sys.argv:
+        v2(True)
+    else: 
+        v2(False)
