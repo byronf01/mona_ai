52a48997bb616f60284d277952745881d82611ad
byronyf01 <byron.fong@gmail.com>
2023-08-04 08:41:46 -0700

    model generates output

--- model.py
+++ model.py
@@ -6,10 +6,11 @@ import sentencepiece as spm
 import torch
 import torch.nn as nn 
 from torch.nn import functional as F
+import tiktoken
 
 # -------- CONSTANTS -------- # 
-# device = "cuda:0" if torch.cuda.is_available() else "cpu"
-device = "cpu"
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
+# device = "cpu"
 VOCAB_SIZE = 50257 + 1 # 51000
 MASKING_TOKEN = 50257 
 LR = 6e-4 # 3e-4
@@ -20,6 +21,7 @@ LR = 3e-4 # 6e-4
 BATCH_SIZE = 14 # 64
 CTX = 200 # 256
 EMBED_DIM = 584
+enc = tiktoken.get_encoding("gpt2")
 # --------------------------- # 
 
 class PositionalEncoding(nn.Module):
@@ -56,7 +58,7 @@ class Model(nn.Module):
         self.ln_final = nn.LayerNorm(EMBED_DIM)
         self.lm_head = nn.Linear(EMBED_DIM, vocab_size)
 
-    def forward(self, x, targets=None):
+    def forward(self, x, src_mask, targets=None):
         
         B, T = x.shape
 
@@ -65,12 +67,11 @@ class Model(nn.Module):
         pos_enb = self.position_embedding_table(torch.transpose(tok_enb, 0, 1).to(device)) # T, B, C
         x = torch.transpose(pos_enb, 1, 0).to(device) # B, T, C
 
-        # Feed into encoder
-        enc_out = self.encoder(x) # -> B, T, C
+        # Feed logits and padding mask into encoder
+        memory, _ = self.encoder((x, src_mask)) # -> B, T, C
 
-        # ??? HOW TO FEED INTO CROSS-ATTENTION? 
-        # Feed into decoder with cross-attention
-        x = self.decoder((x, enc_out)) # does this tuple thing work? 
+        # Feed logits, padding mask and encoder outputs into decoder 
+        x, _, _ = self.decoder((x, memory, src_mask))  
         x = self.ln_final(x)
 
         # finally plug into language model head
@@ -89,14 +90,14 @@ class Model(nn.Module):
         return (logits, loss)
     
     @torch.no_grad()
-    def generate(self, x, max_new_tokens):
+    def generate(self, x, src_mask, max_new_tokens):
         
         # x is (B, T) array of indices in the current context
         for _ in range(max_new_tokens):
             # crop x to last CTX tokens to prevent too much
             x_crop = x[:,-CTX:]
             # get the predictions, ignore loss
-            logits, _ = self(x_crop)
+            logits, _ = self(x_crop, src_mask)
             # focus only on the last element in time dimension
             logits = logits[:, -1, :] # becomes (B, C)
             # apply softmax to get probabilities
@@ -119,11 +120,14 @@ class Encoder(nn.Module):
         self.ln1 = nn.LayerNorm(EMBED_DIM)
         self.ln2 = nn.LayerNorm(EMBED_DIM)
 
-    def forward(self, x):
- 
-        x = self.sa(self.ln1(x)) + x
-        x = self.ffwd(self.ln2(x)) + x
-        return x
+    def forward(self, x: (torch.tensor, torch.tensor)):
+        
+        idx = x[0]
+        mask = x[1] # Padding mask
+
+        idx = self.sa((self.ln1(idx), mask)) + idx
+        idx = self.ffwd(self.ln2(idx)) + idx
+        return (idx, mask)
 
 class Decoder(nn.Module):
     """
@@ -135,7 +139,7 @@ class Decoder(nn.Module):
         super().__init__()
         divided_head_size = EMBED_DIM // HEADS
         self.sa = MultiHeadAttention(divided_head_size, mask=True)
-        self.ca = MultiHeadCrossAttention(divided_head_size)
+        self.ca = CrossAttention(divided_head_size, mask=False)
         self.ffwd = FeedForward(EMBED_DIM)
         self.ln1 = nn.LayerNorm(EMBED_DIM)
         self.ln2 = nn.LayerNorm(EMBED_DIM)
@@ -143,18 +147,17 @@ class Decoder(nn.Module):
 
     def forward(self, x: tuple):
         
-        x, enc_out = x
-        
+        idx, memory, mask = x
         # Masked self-attention
-        x = self.sa(self.ln1(x)) + x
+        idx = self.sa((self.ln1(idx), mask)) + idx
 
         # Cross-attention
-        x = self.ca(self.ln2(x), enc_out) + x 
+        idx = self.ca(self.ln2(idx), memory) + idx 
         
         # Computation with residual connection
-        x = self.ffwd(self.ln3(x)) + x
-        # print(x.shape)
-        return x
+        idx = self.ffwd(self.ln3(idx)) + idx
+        
+        return (idx, memory, mask)
     
 class MultiHeadAttention(nn.Module):
     """
@@ -166,9 +169,13 @@ class MultiHeadAttention(nn.Module):
         self.proj = nn.Linear(EMBED_DIM, EMBED_DIM)
         self.dropout = nn.Dropout(DROPOUT)
 
-    def forward(self, x):
+    def forward(self, x: (torch.tensor, torch.tensor)):
+        
+        idx = x[0]
+        mask = x[1]
+        # print(idx.shape)
 
-        out = torch.concat([h(x) for h in self.heads], dim=-1)
+        out = torch.concat([h((idx, mask)) for h in self.heads], dim=-1)
         # print(out.shape)
         # Linear transformation of outcome 
         out = self.dropout(self.proj(out))
@@ -189,21 +196,27 @@ class Head(nn.Module):
 
     def forward(self, x):
         
-        B, T, C = x.shape
+        idx = x[0]
+        mask = x[1]
+      
+        B, T, C = idx.shape
         # print('Head')
         # print(x.shape)
-        print(x)
 
-        key = self.key(x).to(device)        # (B,T,C)
-        query = self.query(x).to(device)    # (B,T,C)
-        # print(key.shape, query.shape)
+        key = self.key(idx).to(device)        # (B,T,C)
+        query = self.query(idx).to(device)    # (B,T,C)
 
         # compute attention scores (affinities)
         # note scores divided by square root of channels to de-sharpen values for softmax later
 
         # match query against every key
         weights = query @ key.transpose(-2, -1).to(device) * (C ** -0.5) # (B,T,C) @ (B,C,T) -> (B,T,T) 
-        # print(weights.shape)
+        
+        # mask to ignore padding
+        """
+        ARGH !!!!!!!!!
+        weights = weights.masked_fill(mask[:B, :T] == 0, float('-inf')).to(device)
+        """
         # optional mask to ignore future tokens
         if self.mask:
             weights = weights.masked_fill(self.tril[:T, :T] == 0, float('-inf')).to(device) # (B,T,T)
@@ -214,42 +227,43 @@ class Head(nn.Module):
         weights = self.dropout(weights)
 
         # weighted aggregation of the values
-        v = self.value(x).to(device) # (B,T,C)
+        v = self.value(idx).to(device) # (B,T,C)
         out = weights @ v # (B,T,T) @ (B,T,C) --> (B,T,C)
         # print(out.shape)
         return out
     
-class MultiHeadCrossAttention(nn.Module):
+class CrossAttention(nn.Module):
     """
     Multiple heads of cross-attention in parallel
     """
-    def __init__(self, divided_head_size):
+    def __init__(self, divided_head_size, mask=False):
         super().__init__()
-        self.heads = nn.ModuleList([CaHead(divided_head_size) for _ in range(HEADS)])
+        self.heads = nn.ModuleList([CrossHead(divided_head_size, mask) for _ in range(HEADS)])
         self.proj = nn.Linear(EMBED_DIM, EMBED_DIM)
         self.dropout = nn.Dropout(DROPOUT)
 
-    def forward(self, x, enc_out):
+    def forward(self, x, memory):
 
-        out = torch.concat([h.forward(x, enc_out) for h in self.heads], dim=-1)
-        # print(out.shape)
+        out = torch.concat([h.forward(x, memory) for h in self.heads], dim=-1)
+    
         # Linear transformation of outcome 
         out = self.dropout(self.proj(out))
         return out
     
-class CaHead(nn.Module):
+class CrossHead(nn.Module):
     """
     One head of masked self-attention
     """
-    def __init__(self, head_size):
+    def __init__(self, head_size, mask):
         super().__init__()
         self.query = nn.Linear(EMBED_DIM, head_size, bias=False)
         self.key = nn.Linear(EMBED_DIM, head_size, bias=False)
         self.value = nn.Linear(EMBED_DIM, head_size, bias=False)
         self.register_buffer('tril', torch.tril(torch.ones(CTX, CTX).to(device)).to(device))
         self.dropout = nn.Dropout(DROPOUT)
+        self.mask = mask
 
-    def forward(self, x, enc_out):
+    def forward(self, x, memory):
         
         B, T, C = x.shape
         # print('Head')
@@ -259,7 +273,7 @@ class CaHead(nn.Module):
         query = self.query(x).to(device)    # (B,T,C)
 
         # Keys from encoder 
-        key = self.key(enc_out).to(device)  # (B,T,C)
+        key = self.key(memory).to(device)  # (B,T,C)
         
         # compute attention scores (affinities)
         # note scores divided by square root of channels to de-sharpen values for softmax later
@@ -295,3 +309,47 @@ class FeedForward(nn.Module):
     def forward(self, x):
         return self.net(x)
     
+if __name__ == '__main__':
+    train_example = "What is the worst customer service experience you have ever had? "
+    train_example += '<|endoftext|>'
+    idx = enc.encode(train_example, allowed_special={"<|endoftext|>"})
+    idx += [50257 for _ in range(CTX - len(idx))]
+    
+    # Make B x T tensor
+    test_batch = torch.tensor([idx for i in range(BATCH_SIZE)]).to(device)
+
+    # Make the mask based on padding in tensor
+    src_mask = torch.tensor([[0 if token == 50257 else 1 for token in tensor] for tensor in test_batch])
+
+    class TestEncoder(nn.Module):
+
+        def __init__(self):
+            super().__init__()
+            
+            self.token_embedding_table = nn.Embedding(VOCAB_SIZE, EMBED_DIM)
+            self.position_embedding_table = PositionalEncoding(EMBED_DIM, DROPOUT, CTX).to(device)
+            self.encoder = nn.Sequential(*[Encoder() for _ in range(NX)])
+
+        def forward(self, x, targets=None):
+            
+            idx = x[0]
+            mask = x[1]
+
+            tok_enb = self.token_embedding_table(idx) # B, T, C
+            pos_enb = self.position_embedding_table(torch.transpose(tok_enb, 0, 1).to(device)) # T, B, C
+            idx = torch.transpose(pos_enb, 1, 0).to(device) # B, T, C
+
+            # Feed into encoder
+            enc_out = self.encoder((idx, mask)) # -> B, T, C
+
+            return enc_out
+
+    model = TestEncoder().to(device)
+    logits, mask = model((test_batch, src_mask))
+    # print(logits.shape)
+
+    model2 = Model().to(device)
+    logits, loss = model((test_batch, src_mask))
+    # print(logits.shape)
+    wtf = model2.generate(test_batch, src_mask, 100)
+    res = [tensor.tolist() for tensor in wtf]
